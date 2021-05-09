03db4e7ece958dbea5a5fa2810f2d81104c51f87
Aniket Saneet Phanse <aniket.kaps@gmail.com>
2021-05-10 04:15:20 +0900

    style add todo modal

--- src/component/Todo/AddTodo.js
+++ src/component/Todo/AddTodo.js
@@ -52,25 +52,44 @@ const AddTodo = () => {
     return (
         <section className={`${todoDisplay ? 'addtodo-wrapper show': 'addtodo-wrapper'}`} >
             <div className='addtodo-container'>
-                <div className = 'addtodo-headers'>
-                    <p className='addtodo-title'>TodoApp</p>
-                    <button className = 'close-btn' onClick={closeTodoBox}><FaTimes/></button>
+                <div className = 'modal-header'>
+                    <h5 className='modal-title text-white'>Add To-Do Item</h5>
+                    <button type="button" className="close" onClick={closeTodoBox} data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" className="text-white">&times;</span>
+                    </button>                
                 </div>
                 <Alert todoValue={todoValue} emptyInputs={emptyInputs}/>
-                <form onSubmit={handleSubmit}>
-                    <input type='text' className='input title' value={todoValue.title} name='title' placeholder='title' onChange={handleChangeTodo}></input>
-                    <textarea type='text' className='input details' value={todoValue.details} name='details' placeholder='details' onChange={handleChangeTodo}></textarea>
-                    
-                    <label htmlFor="categories">Choose a categories:</label>
-                        <select id="categories" name="category" value={todoValue.category} onChange={handleChangeTodo}>
-                            <option defaultValue={defValue}>Choose Your  categories</option>
-                            {dropList.map((dropItem, index)=>{
-                                return(
-                                    <option value={dropItem}  key={index}>{dropItem}</option>
-                                )
-                            })}
-                        </select>
-                    <button className='addtodo-btn' type='submit'>Confirm<FaPlus/></button>
+                <form onSubmit={handleSubmit} className='mb-2'>
+                    <div className='modal-body'>
+                        <div className='form-group'>
+                            <label for='title'>Title</label>
+                            <input type='text' id='title' className='form-control' value={todoValue.title} placeholder='Enter a title' onChange={handleChangeTodo}></input>
+                        </div>
+                        <div className='form-group'>
+                            <label for='details'>Details</label>
+                            <textarea type='text' id='details' className='form-control' value={todoValue.details} placeholder='Enter details here' onChange={handleChangeTodo}></textarea>
+                        </div>
+                        <div className='form-group'>
+                            <label for="categories">Choose a categories:</label>
+                            <select id="categories" className='form-control' value={todoValue.category} onChange={handleChangeTodo}>
+                                <option defaultValue={defValue}>Default Category</option>
+                                {dropList.map((dropItem, index)=>{
+                                    return(
+                                        <option value={dropItem} key={index}>{dropItem}</option>
+                                    )
+                                })}
+                            </select>
+                        </div>
+                        <div className='form-group mb-2'>
+                            <div class="d-grid gap-2">
+                                <button className='btn btn-primary btn-block mb-2' type='submit'>Confirm</button>
+                                <div className='d-flex justify-content-center'>
+                                    <button className='btn btn-secondary btn-sm' onClick={closeTodoBox}>Cancel</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                                        
                 </form>
                 
             </div>
